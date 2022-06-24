53ff24977d510fb1152fb61ade70efce83fe8a94
Ross Clark <rosscee1998@gmail.com>
2022-06-24 13:46:46 +0100

    add loading state on fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,9 @@ const PARLIAMENT_URL = "https://members-api.parliament.uk/api";
 const Home: NextPage = () => {
   const [constituencyNameInput, setConstituencyNameInput] = useState("");
   const [constituencyNameSuggestions, setConstituencyNameSuggestions] =
-    useState<string[]>([]);
+    useState<string[]>(constituencies);
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const runLandlordCheck = async () => {
@@ -72,12 +73,35 @@ const Home: NextPage = () => {
   };
 
   const handleUserSubmit = async () => {
+    setIsLoading(true);
     if (constituencies.indexOf(constituencyNameInput) === -1) {
       setIsError(true);
       return false;
     }
     await runLandlordCheck();
   };
+
+  if (isLoading)
+    return (
+      <div className="text-center min-h-screen">
+        <svg
+          role="status"
+          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+      </div>
+    );
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -91,7 +115,7 @@ const Home: NextPage = () => {
           Is your MP a <span className="text-red-500">private landlord</span>?
         </h1>
         <br />
-        <p className="mt-3 text-2xl">Enter your MP&apos;s name to find out:</p>
+        <p className="mt-3 text-2xl">Enter your constituency to find out:</p>
         <form
           className="self-stretch items-center"
           onSubmit={async (e) => {
@@ -105,7 +129,7 @@ const Home: NextPage = () => {
                 className={`border rounded py-2 px-3 mt-5 h-10 w-full text-center ${
                   isError && "border-red-500"
                 }`}
-                type="text"
+                type="search"
                 name="constituency-name"
                 list="constituency-names"
                 enterKeyHint="search"
