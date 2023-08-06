fba8fabee65187b9cba1ca01bb3d8d8829f1e6bb
Ross Clark <rosscee1998@gmail.com>
2023-08-06 23:16:33 +0100

    Use skeleton for loading state; extend linter with further plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+    "plugin:react/jsx-runtime",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react-refresh", "react", "jest-dom", "testing-library"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, Input, Text } from "@chakra-ui/react";
+import { Button, Heading, Input, Skeleton, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import getMember from "../utils/getMember";
@@ -7,17 +7,11 @@ export default function App() {
   const [currentInput, setCurrentInput] = useState("");
   const [submittedInput, setSubmittedInput] = useState("");
 
-  const { isInitialLoading, isError, error, data } = useQuery<string, Error>({
+  const { isInitialLoading, isError, data } = useQuery({
     queryKey: [submittedInput],
     queryFn: () => getMember(submittedInput),
     enabled: !!submittedInput,
   });
-
-  function handleSubmit() {
-    setSubmittedInput(currentInput);
-  }
-
-  if (isInitialLoading) return <Text>Loading...</Text>;
 
   return (
     <>
@@ -27,13 +21,16 @@ export default function App() {
         value={currentInput}
         onChange={({ target: { value } }) => setCurrentInput(value)}
         onKeyDown={({ key }) => {
-          if (key === "Enter") handleSubmit();
+          if (key === "Enter") setSubmittedInput(currentInput);
         }}
         placeholder="SW1A 1AA"
       />
-      <Button onClick={() => handleSubmit()}>Submit</Button>
-      {data?.nameDisplayAs ? <Heading>{data.nameDisplayAs}</Heading> : null}
-      {isError ? <Heading>Invalid postcode</Heading> : null}
+      <Button onClick={() => setSubmittedInput(currentInput)}>Submit</Button>
+      <Skeleton isLoaded={!isInitialLoading} height="2rem">
+        {data?.nameDisplayAs ? <Heading>{data.nameDisplayAs}</Heading> : null}
+        {isError ? <Heading>Invalid postcode</Heading> : null}
+      </Skeleton>
     </>
+    // </Skeleton>
   );
 }
